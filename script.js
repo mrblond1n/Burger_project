61943047e6effdfd900e5f4ddd0324cdbf5b2438
MrBlond1n <nikita.kirdiapin@gmail.com>
2019-05-29 16:11:31 +0300

    add functional for slider

--- script.js
+++ script.js
@@ -69,6 +69,37 @@ for (let i = 0; i < memberCard.length; i++) {
       memberCard[i].classList.add('member__card--active');
     }
   }); 
-}
+};
 
+// SLIDER BURGER
+
+const toLeft = document.querySelector('.burger-slider__arrow-left'),
+toRight = document.querySelector('.burger-slider__arrow-right'),
+sliderList = document.querySelector('.burger-slider__list'),
+sliderItem = document.querySelectorAll('.burger-slider__item'),
+sliderLength = sliderItem.length - 1;
+console.log(sliderLength * -100);
+
+let count = 0;
+
+toRight.addEventListener('click', function() {
+  let toRight = -100;
+  
+  if (count > sliderLength * -100) {
+    count += toRight;
+  };
+  
+  sliderList.style.transform = 'translateX(' + count + '%)';
+});
+
+
+toLeft.addEventListener('click', function() {
+  let toLeft = 100;
+
+  if(count < 0) {
+    count += toLeft;
+  };
+
+  sliderList.style.transform = 'translateX(' + count + '%)';
+});
 
